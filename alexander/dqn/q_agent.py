1ff7901c4ed5d473c3b54e15a27fc0a8491b0485
alexanderbonnet <alex.bonnet.us@gmail.com>
2022-03-22 14:34:26 +0100

    update agent

--- alexander/dqn/q_agent.py
+++ alexander/dqn/q_agent.py
@@ -7,9 +7,8 @@ from dqn.hiv_patient import HIVPatient
 
 
 class Agent:
-    def __init__(self, patient: HIVPatient, replay_buffer: ReplayBuffer) -> None:
+    def __init__(self, patient: HIVPatient) -> None:
         self.patient = patient
-        self.replay_buffer = replay_buffer
 
         self.state = None
         self.reset()
@@ -17,25 +16,31 @@ class Agent:
     def reset(self) -> None:
         self.state = self.patient.reset()
 
-    def get_action(self, net: nn.Module, epsilon: float, device: str):
+    def get_action(self, net: nn.Module, epsilon: float, device: str) -> int:
         if np.random.random() > epsilon:
             action = self.patient.sample_action_space()
         else:
             state = torch.tensor(np.array([self.state]), device=device)
 
             q_values = net(state)
-            _, action = torch.max(q_values, 1)
+            _, action = torch.max(q_values, dim=1)
             action = int(action.item())
         return action
 
-    def play_step(self, net: nn.Module, epsilon: float, device: str):
+    def play_step(
+        self,
+        net: nn.Module,
+        epsilon: float,
+        device: str,
+        replay_buffer: ReplayBuffer,
+    ) -> float:
         with torch.no_grad():
             action = self.get_action(net, epsilon, device)
 
             # do step in the environment
-            new_state, reward, _, _ = self.patient.step(action)
+            new_state, reward, done, _ = self.patient.step(action)
             exp = Experience(self.state, action, reward, done, new_state)
-            self.replay_buffer.append(exp)
+            replay_buffer.append(exp)
 
             self.state = new_state
             return reward
